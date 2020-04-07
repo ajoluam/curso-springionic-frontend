d2e7d7abc50db7029014a84f0529de3057edbd7b
Alexandre Amorim <ajoluam@hotmail.com>
2020-04-07 16:25:30 -0300

    Navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,19 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //Isso aqui é ma injeção de dependência através do construtor
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login(){
+    //Em Typescript todo objeto tem que ser preceddo pelo THIS, não posso chamar o objeto diretamente
+    //Como o push empilha a página uma em cima da outra, ele coloca automaticamente uma seta para voltar
+    //this.navCtrl.push('CategoriasPage');
+
+    //Com o setRoot ele não empilha a página então não aparece a setinha para voltar
+    //aparece só o menu 
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
